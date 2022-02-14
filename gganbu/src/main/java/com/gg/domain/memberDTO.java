df8e0be9b350eea506472032d086a59d75d83e3b
DESKTOP-9UTR83B\Laptop <bhyun2013@naver.com>
2022-02-14 15:17:19 +0900

    멤버 DTO, Mapper, Service 추가 (깡통)

---
+++ gganbu/src/main/java/com/gg/domain/memberDTO.java
@@ -0,0 +1,8 @@
+package com.gg.domain;
+
+import lombok.Data;
+
+@Data
+public class memberDTO {
+    String mem_id, mem_pass, mem_name, mem_email1, mem_email2, mem_hp, mem_birth, mem_gender, mem_date, mem_status;
+}
---
+++ gganbu/src/main/java/com/gg/mapper/memberMapper.java
@@ -0,0 +1,5 @@
+package com.gg.mapper;
+
+public interface memberMapper {
+
+}
