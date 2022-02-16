5e7e04d2ac58989b54ada01d2608ff9e086da4d1
CYI\cyi <wlfkfqkfrhkd93@gmail.com>
2022-02-16 15:40:46 +0900

    mapper 로그인 대소문자 변경

--- gganbu/src/main/java/com/gg/mapper/LoginMapper.java
+++ gganbu/src/main/java/com/gg/mapper/LoginMapper.java
@@ -5,7 +5,7 @@ import org.apache.ibatis.annotations.Mapper;
 import com.gg.domain.MemberDTO;
 
 @Mapper
-public interface loginMapper {
+public interface LoginMapper {
     
     public MemberDTO loginAction(MemberDTO dto);
     
--- gganbu/src/main/java/com/gg/service/LoginServiceImpl.java
+++ gganbu/src/main/java/com/gg/service/LoginServiceImpl.java
@@ -4,13 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gg.domain.MemberDTO;
-import com.gg.mapper.loginMapper;
+import com.gg.mapper.LoginMapper;
 
 @Service
 public class LoginServiceImpl implements LoginService {
 	
 	@Autowired
-	private loginMapper loginMapper;
+	private LoginMapper loginMapper;
 	
 	@Override
 	public MemberDTO loginAction(MemberDTO dto) {
