5480be468438685e0b216152126cb8949130140d
Laptop <bhyun2013@naver.com>
2022-02-26 21:12:21 +0900

    깐부평 유효성 추가, 수정 많이 필요

--- gganbu/src/main/resources/static/js/gganbucomment.js
+++ gganbu/src/main/resources/static/js/gganbucomment.js
@@ -1,10 +1,31 @@
 /**
  * 깐부평 전용
+ let data = (${gganbu_comment_content}).val();
  */
+ let comment = [['[[${gganbu_comment_content}]]']];
+ var re2 = /^.{1,20}$/;
+ 
  
  function insertORupdateGganbuComment(url){
+    
+    //alert('111');
+    let you = document.getElementById("gganbu_comment_content");
     let insupdsubmit = $("#insertORupdateForm").serialize();
     
+    GBCMsg.innerHTML = "";
+    
+    //alert($('#gganbu_comment_content').val());
+    if(!re2.test($('#gganbu_comment_content').val())){
+        
+    //alert('222');
+    
+    GBCMsg.style.color = "#FF1493";
+    gganbu_comment_content.style.border = "2px solid #FF1493";
+    GBCMsg.innerHTML = "유효한 양식이 아닙니다";
+    return false;
+    
+    }else {
+        
     $.ajax({
         url : url,
         type : 'POST',
@@ -17,4 +38,11 @@
             $('#test').html(data);
         }
     });
+        
+    GBCMsg.innerHTML = "";
+    
+    }
+    
+    //alert('444');
+    
 }
