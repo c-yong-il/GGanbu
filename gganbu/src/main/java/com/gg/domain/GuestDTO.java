deedc2d304e2926dc771a458ad51ab723c19aa17
LAPTOP-DN358BNA\imreu <imreu@naver.com>
2022-02-27 14:14:01 +0900

    비밀글 진행중 30%

--- gganbu/src/main/java/com/gg/domain/GuestDTO.java
+++ gganbu/src/main/java/com/gg/domain/GuestDTO.java
@@ -10,5 +10,5 @@ import lombok.Setter;
 public class GuestDTO extends Criteria {
 
     private int book_num, rno;
-    private String mem_id2, book_content, book_date, book_updatedate, book_status;
+    private String mem_id2, book_content, book_date, book_updatedate, book_secret, book_status;
 }
