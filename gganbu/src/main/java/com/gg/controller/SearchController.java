54370431e2b8a924ed0ab1fb95e4d3f595bf309e
DESKTOP-B3P3G7L\frost <bhhj4640@naver.com>
2022-03-02 12:07:31 +0900

    깐부관리에서 깐부수락날짜, 깐부수락요청날짜에 따라 정렬안되던거 오류 수정

--- gganbu/src/main/java/com/gg/controller/SearchController.java
+++ gganbu/src/main/java/com/gg/controller/SearchController.java
@@ -23,25 +23,7 @@ public class SearchController {
     SearchService searchservice;
 
     @GetMapping(value = "/search/search")
-    public String search(HttpSession session, Model model) {
-        MemberDTO dto = (MemberDTO) session.getAttribute("loginUser");
-        //System.out.println(dto.getMem_id());
-
-        //        List<MgmDTO> list = searchservice.gganbuList(dto.getMem_id());
-
-        //        System.out.println("id2=" + list.get(0).getMem_id2());
-        //        System.out.println("id2=" + list.get(1).getMem_id2());
-        //        System.out.println("리스트사이즈=" + list.size());
-        //        for (int i = 0; i < list.size(); i++) {
-        //            if (list.get(i).getMem_id().equals(dto.getMem_id())) {
-        //                model.addAttribute("mem_id", list.get(i).getMem_id2());
-        //                System.out.println("list.get(i).getMem_id2()" + list.get(i).getMem_id2());
-        //            } else {
-        //                model.addAttribute("mem_id", list.get(i).getMem_id());
-        //                System.out.println("list.get(i).getMem_id()" + list.get(i).getMem_id());
-        //            }
-        //        }
-        //        model.addAttribute("list", list);
+    public String search() {
 
         return "search/search";
     }
@@ -54,14 +36,12 @@ public class SearchController {
 
         int ggCheck = searchservice.ggCheck(dto.getMem_id(), yid);
         int result = -1;
-        if (ggCheck == 2) { //insert
+        if (ggCheck == 2) { //insert 
             searchservice.insertGganbu(dto.getMem_id(), yid);
             result = 0;
-            //System.out.println("깐부 신청 insert");
-        } else if (ggCheck == 1) {
+        } else if (ggCheck == 1) { //이미 깐부관계
             result = 1;
-            //System.out.println("이미 깐부야!!");
-        } else if (ggCheck == 0) {
+        } else if (ggCheck == 0) { //깐부 수락 대기 중
             result = 2;
         }
         return result;
@@ -73,41 +53,9 @@ public class SearchController {
         //System.out.println("검색어=" + search);
         List<MemberDTO> gglist = searchservice.search(search.trim());
         MemberDTO dto = (MemberDTO) session.getAttribute("loginUser"); //로그인유저랑 search해서 나온 사람이랑 깐부인지 확인해서 뷰에 보내려했으나 뷰에서 받으면 이중for문 돼서 거지같아짐
-        //        System.out.println("gglist size=" + gglist.size());
-        //        System.out.println("1번=" + gglist.get(0).getMem_id());
-        //        System.out.println("2번" + gglist.get(1).getMem_id());
-        //        System.out.println("3번" + gglist.get(2).getMem_id());
 
-        //        int ggCheck = -1;
-        //        List<Integer> ggCheckList = new ArrayList<>();
-        //        if (gglist.size() != 0) {
-        //            String myid = dto.getMem_id();
-        //            for (int i = 0; i < gglist.size(); i++) {
-        //                String yid = gglist.get(i).getMem_id();
-        //
-        //                ggCheck = searchservice.ggCheck(myid, yid);
-        //                ggCheckList.add(ggCheck);
-        //            }
-        //        }
-
-        //        System.out.println("ggCheck0=" + ggCheckList.get(0));
-        //        System.out.println("ggCheck1=" + ggCheckList.get(1));
-        //        System.out.println("ggCheck2=" + ggCheckList.get(2));
-        //        System.out.println("ggCheck3=" + ggCheckList.get(3));
-
-        //model.addAttribute("ggCheckList", ggCheckList);
-        /*
-         * System.out.println("id1=" + gglist.get(0).getMem_id());
-         * System.out.println("id2=" + gglist.get(0).getMem_id2());
-         * System.out.println("관계=" + gglist.get(0).getGganbu_relation());
-         * System.out.println("id1=" + gglist.get(1).getMem_id());
-         * System.out.println("id2=" + gglist.get(1).getMem_id2());
-         * System.out.println("관계=" + gglist.get(1).getGganbu_relation());
-         */
         model.addAttribute("gglist", gglist);
 
         return "search/search";
-
     }
-
 }
